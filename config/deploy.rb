685dac73f16ec7929416bb61465ead1e3ef0b5f5
kim sia sim <kimcity@gmail.com>
2010-10-28 22:53:54 +0800

    changes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,17 @@
+# set the arbitrary application name
+set :application, "ombi60.com"
+# set the reposiotry url 
+set :repository, "git-ombi@pl3.projectlocker.com:ombi60_on_git.git"
+
 # Only deploy the master branch
 set :branch, "master"
 # Use git to deploy. You can also set this to 'svn'
 set :scm, :git
 # Keep Git quiet
 set :scm_verbose, false
+
+# set server user account responsible
+set :user, "deploy"
 
 ## Deploy Settings
 # Deploy via a remote repository cache. In git's case, it 
@@ -15,39 +23,36 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 ## SSH Options
-# Deploy as this username the user in the server that is responsible for deployment
-ssh_options[:username] = 'ubuntu'
+
 # SSH Agent forwarding, sends my personal keys for usage by git when deploying.
 ssh_options[:forward_agent] = true
+
+ssh_options[:keys] = %w( /home/kei/Dropbox/amazon\ aws/ombi60_key.pem )
 
 
 # setting for production environment
 task :production do
-  set :application, "ombi60.com"
-  set :repository, "git-ombi@pl3.projectlocker.com:ombi60_on_git.git"
-  
+  # If you aren't deploying to /u/apps/#{application} on the target
+  # servers (which is the default), you can specify the actual location
+  # via the :deploy_to variable:
+  set :deploy_to, "/var/www/ombi60.com"
 end
 
 
 # setting for production environment
 task :staging do
-  set :application, "ombi60.biz"
-  set :repository, "git-ombi@pl3.projectlocker.com:ombi60_on_git.git"
-  after('restore_staging_database')
+  # If you aren't deploying to /u/apps/#{application} on the target
+  # servers (which is the default), you can specify the actual location
+  # via the :deploy_to variable:
+  set :deploy_to, "/var/www/ombi60.biz"
+  #after('restore_staging_database')
 end
-
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
-
 
 # more applicable for rails app
 # for increasing load
-role :app, "ombi60.com"
-role :web, "ombi60.com"
-role :db,  "ombi60.com", :primary => true
+role :app, "ombi60.biz"
+role :web, "ombi60.biz"
+role :db,  "ombi60.biz", :primary => true
 
 
 #Deployment tasks
@@ -65,20 +70,15 @@ end
 
 
 namespace :clear_cache do
-  desc "Blow up all the cache files CakePHP uses, ensuring a clean restart."
+  desc <<-DESC
+    Blow up all the cache files CakePHP uses, ensuring a clean restart.
+  DESC
   task :default do
-    # Remove ONLY files from TMP but leave the folders alone.
-    run "rm -f #{shared_path}/app/tmp/*"
-    
-    # Remove ONLY files from TMP/cache but leave the folders alone.
-    run "rm -f #{shared_path}/app/tmp/cache/*"
-    
-    # Remove ONLY files from TMP/cache/models, TMP/cache/views, TMP/cache/persistent, but leave the folders alone.
-    run "rm -f #{shared_path}/app/tmp/cache/{models/*,views/*,persistent/*}"
-    
-    # Remove ONLY files from TMP/cache/models, TMP/cache/views, TMP/cache/persistent, but leave the folders alone.
-    run "rm -f #{shared_path}/app/tmp/{sessions,tests}"
-
+    # Remove absolutely everything from TMP
+    run "rm -rf #{shared_path}/tmp/*"
+ 
+    # Create TMP folders
+    run "mkdir -p #{shared_path}/tmp/{cache/{models,persistent,views},sessions,logs,tests}"
   end
 end
 
