7f04be9c04e45d34f301ba0fe2f991f99ad48a18
kimsia <kimcity@gmail.com>
2011-03-28 13:53:46 +0800

    addede new model called shop_setting for app folder

---
+++ app/models/shop_setting.php
@@ -0,0 +1,28 @@
+<?php
+class ShopSetting extends AppModel {
+	var $name = 'ShopSetting';
+	var $validate = array(
+		'checkout_language' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Shop' => array(
+			'className' => 'Shop',
+			'foreignKey' => 'shop_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
+?>
---
+++ app/tests/fixtures/shop_setting_fixture.php
@@ -0,0 +1,36 @@
+<?php
+/* ShopSetting Fixture generated on: 2011-03-28 05:03:54 : 1301291454 */
+class ShopSettingFixture extends CakeTestFixture {
+	var $name = 'ShopSetting';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'shop_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'timezone' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'unit_system' => array('type' => 'string', 'null' => true, 'default' => 'metric', 'length' => 50),
+		'currency' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100),
+		'money_html_currency' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'money_html' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'money_email_currency' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'money_email' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'checkout_language' => array('type' => 'integer', 'null' => true, 'default' => '1', 'length' => 3),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'shop_id' => 1,
+			'timezone' => 'Lorem ipsum dolor sit amet',
+			'unit_system' => 'Lorem ipsum dolor sit amet',
+			'currency' => 'Lorem ipsum dolor sit amet',
+			'money_html_currency' => 'Lorem ipsum dolor sit amet',
+			'money_html' => 'Lorem ipsum dolor sit amet',
+			'money_email_currency' => 'Lorem ipsum dolor sit amet',
+			'money_email' => 'Lorem ipsum dolor sit amet',
+			'checkout_language' => 1
+		),
+	);
+}
+?>
