b12513ee21290e5205148fe9ee5c404cd164e067
kimsia <kimcity@gmail.com>
2011-06-15 13:24:51 +0800

    alter the Shop model so that it correctly extracts the visible_product_count from the database

--- app/models/shop.php
+++ app/models/shop.php
@@ -422,7 +422,7 @@ class Shop extends AppModel {
 			      'primary_domain'=> $shopInstance['Shop']['primary_domain'] . '/',
 			      'permanent_domain'=>$shopInstance['Shop']['permanent_domain'] . '/',
 			      'email'=>$shopInstance['Shop']['email'],
-			      'products_count'=>$shopInstance['Shop']['product_count'],
+			      'products_count'=>$shopInstance['Shop']['visible_product_count'],
 			      
 			      'collections_count'=>$shopInstance['Shop']['product_group_count'],
 			      'currency'=>$shopInstance['ShopSetting']['currency'],
