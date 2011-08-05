843047d2a0631af4b17c1fc179b5430aea3a73bc
Francis Besset <francis.besset@gmail.com>
2011-08-05 15:14:00 +0200

    Added comment in ombi60_environment

--- app/plugins/twig_view/libs/ombi60_environment.php
+++ app/plugins/twig_view/libs/ombi60_environment.php
@@ -29,12 +29,19 @@ class Ombi60_Twig_Environment extends Twig_Environment
      */
     public function loadTemplate($name)
     {
+        // $name = /NameOfThemeFolder/NameOfCurrentTheme/template.tpl
+
+        // If the template path starts by /NameOfThemeFolder/ or NameOfThemeFolder/ == TwigView::THEME_FOLDER constant
         if (preg_match('#^/?'.preg_quote($this->themeFolder).'/(.*)$#', $name, $match)) {
+            // If the template belongs to another theme
             if (!preg_match('#^'.preg_quote($this->theme).'#', $match[1])) {
+                // throw an InvalidArgumentException
                 throw new InvalidArgumentException(sprintf('The template "%s" cannot be included for the theme "%s"', $name, $this->theme));
             }
         }
 
+        // else if the template belongs at the same theme or does not belong to a theme
+        // return the result of parent::loadTemplate()
         return parent::loadTemplate($name);
     }
 }
