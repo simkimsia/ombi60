7d081d965792fea6dfeb08388dc590f048fba309
kimsia <kimcity@gmail.com>
2011-09-20 23:10:28 +0800

    add in edit_paypal_payment as an action where we set Security->validatePost = false

--- app/controllers/payments_controller.php
+++ app/controllers/payments_controller.php
@@ -9,7 +9,8 @@ class PaymentsController extends AppController {
 	
 	function beforeFilter() {
 		parent::beforeFilter();
-		if ($this->action == 'admin_add_paypal_payment') {
+		if ($this->action == 'admin_add_paypal_payment' ||
+			$this->action == 'admin_edit_paypal_payment' ) {
 			$this->Security->validatePost = false;			
 		}
 
