e9b15e38e619a8e3add24b58a445c533cd0e7408
Alejandro Ibarra <ajibarra@gmail.com>
2011-09-21 21:14:57 +0200

    Merchant fixture

--- app/Test/Fixture/MerchantFixture.php
+++ app/Test/Fixture/MerchantFixture.php
@@ -1,19 +1,37 @@
 <?php
-/* Merchant Fixture generated on: 2010-04-17 12:04:18 : 1271507538 */
-class MerchantFixture extends CakeTestFixture {
-	var $name = 'Merchant';
+/* Merchant Fixture generated on: 2011-09-21 18:53:24 : 1316631204 */
 
-	var $import = 'Merchant';
-	
-	var $records = array(
-		array(
-			'id' => 1,
-			'shop_id' => 1,
-			'user_id' => 1,
-			'owner' => true,
-			
-		),
+/**
+ * MerchantFixture
+ *
+ */
+class MerchantFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
+		'owner' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'collate' => NULL, 'comment' => ''),
+		'shop_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
 
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => '1',
+			'owner' => 1,
+			'shop_id' => '2',
+			'user_id' => '1'
+		),
+	);
 }
-?>
