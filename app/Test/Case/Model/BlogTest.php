8a2378d3fc16cdb85e6eaa494c6fe60a315ca765
Alejandro Ibarra <ajibarra@gmail.com>
2011-09-22 13:43:17 +0200

    Blog model test generated

--- app/Test/Case/Model/BlogTest.php
+++ app/Test/Case/Model/BlogTest.php
@@ -1,18 +1,49 @@
 <?php
-/* Blog Test cases generated on: 2010-09-02 04:09:39 : 1283395059*/
-App::import('Model', 'Blog');
+/* Blog Test cases generated on: 2011-09-22 09:42:11 : 1316684531*/
+App::uses('Blog', 'Model');
 
+/**
+ * Blog Test Case
+ *
+ */
 class BlogTestCase extends CakeTestCase {
-	var $fixtures = array('app.blog', 'app.post');
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array('app.blog', 'app.saved_theme', 'app.user', 'app.shop_setting', 'app.shop', 'app.domain', 'app.post', 'app.comment', 'app.link', 'app.link_list', 'app.product', 'app.webpage');
+	
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
 
-	function startTest() {
-		$this->Blog =& ClassRegistry::init('Blog');
+		$this->Blog = ClassRegistry::init('Blog');
 	}
 
-	function endTest() {
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
 		unset($this->Blog);
 		ClassRegistry::flush();
+
+		parent::tearDown();
+	}
+
+/**
+ * testGetTemplateVariable method
+ *
+ * @return void
+ */
+	public function testGetTemplateVariable() {
+
 	}
 
 }
-?>
