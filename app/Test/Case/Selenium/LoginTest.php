28716713f27b62124b3a9058cd5136ba7d4151be
Alejandro Ibarra <ajibarra@gmail.com>
2011-09-22 14:43:16 +0200

    Login test with selenium and phpunit

---
+++ app/Test/Case/Selenium/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+ 
+class WebTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+    protected function setUp() {
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://shop001.ombi60.localhost/');
+    }
+ 
+    public function testTitle() {
+		//Testing public site
+		$this->open('http://shop001.ombi60.localhost/');
+		$this->assertTitle('shop001 — Welcome');
+		//Testing admin site
+		$this->open('http://shop001.ombi60.localhost/admin');
+		$this->assertTitle('OMBI60: Open My Business in 60 Seconds: Merchant Login');
+    }
+    
+    public function testLogin() {
+    	$this->open('http://shop001.ombi60.localhost/admin');
+    	$this->type('id=UserEmail', 'owner@shop001.com');
+    	$this->type('id=UserPassword', '5597701');
+    	$this->clickAndWait('css=input[type="submit"]');
+    	$this->assertTitle('OMBI60: Open My Business in 60 Seconds: Merchants');
+    }
+    
+}
+?>
