947085dfe8a1b8f13248d2ffc69cee14711ece3b
kimsia <kimcity@gmail.com>
2011-10-12 21:24:40 +0800

    Include CountryTest in AllModel

--- app/Test/Case/Model/CountryTest.php
+++ app/Test/Case/Model/CountryTest.php
@@ -1,18 +1,90 @@
 <?php
-/* Country Test cases generated on: 2010-09-08 08:09:41 : 1283927081*/
-App::import('Model', 'Country');
+/* Cart Test cases generated on: 2011-09-22 09:25:06 : 1316683506*/
+App::uses('User', 'Model');
+App::uses('Shop', 'Model');
+App::uses('Country', 'Model');
 
+/**
+ * Country Test Case
+ *
+ */
 class CountryTestCase extends CakeTestCase {
-	var $fixtures = array('app.country', 'app.shipping_rate', 'app.price_based_rate', 'app.weight_based_rate');
+	/**
+	 * Fixtures
+	 *
+	 * @var array
+	 *
+	 **/
+	public $fixtures = array(
+		'app.shop',  'app.domain',
+		'app.shop_setting', 'app.language',
+		'app.user', 'app.group',
+		'app.merchant', 'app.customer', 'app.casual_surfer',
+		'app.cart', 'app.cart_item',
+		'app.order', 'app.order_line_item', 'app.address', 
+		'app.product', 'app.product_image', 'app.wishlist', 
+		'app.variant', 'app.variant_option', 'app.products_in_group', 'app.product_group',  
+		'app.product_type', 'app.vendor',
+		'app.smart_collection_condition',
+		'app.webpage', 'app.page_type', 
+		'app.link_list', 'app.link', 
+		'app.blog', 'app.post', 'app.comment', 
+		'app.payment', 'app.shops_payment_module', 'app.payment_module',
+		'app.log', 'app.saved_theme',
+ 		'app.country',
+		'app.shipment', 'app.shipping_rate', 'app.shipped_to_country',	
+		'app.price_based_rate', 'app.weight_based_rate'	
+	);
 
-	function startTest() {
-		$this->Country =& ClassRegistry::init('Country');
+
+	/**
+	 * setUp method
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Country = ClassRegistry::init('Country');
+		
+		// setting up Shop and User singleton
+		$this->Shop 	= ClassRegistry::init('Shop');
+		$this->User 	= ClassRegistry::init('User');
+		
+		$cachedShopId = Shop::get('Shop.id');
+		
+		if ($cachedShopId != 2) {
+			$testShop = $this->Shop->getById(2);
+			Shop::store($testShop);
+		}
+		
+		$cachedUserId = User::get('User.id');
+		
+		if($cachedUserId != 2) {
+			User::store($this->User->read(null, 2));
+		}
+		// this is to allow User singleton to work properly
+		// look at AppController beforeFilter
+		Configure::write('run_test', true);
+		
 	}
 
-	function endTest() {
+	/**
+	 * tearDown method
+	 *
+	 * @return void
+	 */
+	public function tearDown() {
 		unset($this->Country);
+		unset($this->Shop);
+		unset($this->User);
 		ClassRegistry::flush();
-	}
 
+		parent::tearDown();
+	}
+	
+	public function testNoTests() {
+		// dummy placeholder because no tests for Country Model and we need to pass the AllModel
+	}
+		
 }
-?>
