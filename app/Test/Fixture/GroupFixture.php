8d4860c4adeb1303bf239f7d9d46167235900895
Alejandro Ibarra <ajibarra@gmail.com>
2011-10-12 16:26:44 +0200

    Changing fixtures import->schema

--- app/Test/Fixture/GroupFixture.php
+++ app/Test/Fixture/GroupFixture.php
@@ -1,10 +1,31 @@
 <?php
-/* Group Fixture generated on: 2010-04-21 11:04:19 : 1271849179 */
+/* Group Fixture generated on: 2011-10-12 14:26:04 : 1318429564 */
+
+/**
+ * GroupFixture
+ *
+ */
 class GroupFixture extends CakeTestFixture {
-	var $name = 'Group';
 
-	var $import = array('model' => 'Group');
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
 
+/**
+ * Records
+ *
+ * @var array
+ */
 	var $records = array(
 		array(
 			'id' => 1,
@@ -32,4 +53,3 @@ class GroupFixture extends CakeTestFixture {
 		),
 	);
 }
-?>
--- app/Test/Fixture/ThemeFixture.php
+++ app/Test/Fixture/ThemeFixture.php
@@ -1,20 +1,49 @@
 <?php
-/* Theme Fixture generated on: 2010-05-26 08:05:55 : 1274863915 */
+/* Theme Fixture generated on: 2011-10-12 14:25:33 : 1318429533 */
+
+/**
+ * ThemeFixture
+ *
+ */
 class ThemeFixture extends CakeTestFixture {
-	var $name = 'Theme';
-	var $import = array('model' => 'Theme');
 
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
+		'description' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
+		'author' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'available_for_all' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'collate' => NULL, 'comment' => ''),
+		'folder_name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
+		'shop_id' => array('type' => 'integer', 'null' => true, 'default' => '0', 'collate' => NULL, 'comment' => ''),
+		'price' => array('type' => 'float', 'null' => true, 'default' => '0.00', 'length' => '7,2', 'collate' => NULL, 'comment' => ''),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
 
-	var $records = array(
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
 		array(
 			'id' => 1,
 			'name' => 'Lorem ipsum dolor sit amet',
 			'description' => 'Lorem ipsum dolor sit amet',
 			'author' => 'Lorem ipsum dolor sit amet',
-			'created' => '2010-05-26 08:51:55',
-			'modified' => '2010-05-26 08:51:55',
-			'available_for_all' => 1
+			'created' => '2011-10-12 14:25:33',
+			'modified' => '2011-10-12 14:25:33',
+			'available_for_all' => 1,
+			'folder_name' => 'Lorem ipsum dolor sit amet',
+			'shop_id' => 1,
+			'price' => 1
 		),
 	);
 }
-?>
