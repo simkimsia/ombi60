011950850a60e0d3e8a42711e050ecb377f7ed12
Alejandro Ibarra <ajibarra@gmail.com>
2011-11-10 16:54:45 -0430

    Completed PaymentsComponent test

--- app/Plugin/Payments/Test/Case/Controller/Component/PaymentsComponentTest.php
+++ app/Plugin/Payments/Test/Case/Controller/Component/PaymentsComponentTest.php
@@ -2,6 +2,10 @@
 /* Payments Test cases generated on: 2011-11-10 15:31:16 : 1320939076*/
 App::uses('PaymentsComponent', 'Payments.Controller/Component');
 App::uses('ComponentCollection', 'Controller');
+App::uses('Controller', 'Controller');
+App::uses('PaypalExpressComponent', 'Payments.Controller/Component');
+require_once App::pluginPath('Payments') . 'Vendor' . DS . 'AktiveMerchant'.DS.'lib'.DS.'merchant'.DS.'billing'.DS.'Response.php';
+require_once App::pluginPath('Payments') . 'Vendor' . DS . 'AktiveMerchant'.DS.'lib'.DS.'merchant'.DS.'billing'.DS.'gateways'.DS.'paypal'.DS.'PaypalExpressResponse.php';
 
 /**
  * PaymentsComponent Test Case
@@ -16,7 +20,13 @@ class PaymentsComponentTestCase extends CakeTestCase {
 	public function setUp() {
 		parent::setUp();
 		$this->ComponentCollection = new ComponentCollection();
+		$paypalExpress = $this->getMock('PaypalExpressComponent', array('setupPurchase', 'purchase', 'getUrlForToken'), array(new ComponentCollection(), array()));
+		$controller = $this->getMock('Controller', array('redirect'));
+		$paypalExpressResponse = new Merchant_Billing_PaypalExpressResponse(true, 'Success Payment', array('parameter' => 'parameter_value', 'TOKEN' => 'TOKEN'), array());
+		$paypalExpress->response = $paypalExpressResponse;
+		$this->ComponentCollection->set('PaypalExpress', $paypalExpress);
 		$this->Payments = new PaymentsComponent($this->ComponentCollection);
+		$this->Payments->initialize($controller);
 	}
 
 /**
@@ -26,7 +36,6 @@ class PaymentsComponentTestCase extends CakeTestCase {
  */
 	public function tearDown() {
 		unset($this->Payments);
-
 		parent::tearDown();
 	}
 
@@ -36,9 +45,9 @@ class PaymentsComponentTestCase extends CakeTestCase {
  * @return void
  */
 	public function testSetupPurchase() {
-		$paypalExpress = $this->getMock('PaypalExpressComponent', array('setupPurchase'));
-		$paypalExpress->expects($this->once())->method('setupPurchase')->with($this->equalTo(array('amount' => 10)));
+		$this->Payments->PaypalExpress->expects($this->once())->method('setupPurchase')->with($this->equalTo(array('amount' => 10)));
 		$this->Payments->setupPurchase('Paypal Express Checkout', array('amount' => 10));
+		//Add more tests for future gateways
 	}
 
 /**
@@ -47,7 +56,9 @@ class PaymentsComponentTestCase extends CakeTestCase {
  * @return void
  */
 	public function testPurchase() {
-
+		$this->Payments->PaypalExpress->expects($this->once())->method('purchase')->with($this->equalTo(array('amount' => 10)));
+		$this->Payments->purchase('Paypal Express Checkout', array('amount' => 10));
+		//Add more tests for future gateways
 	}
 
 /**
@@ -56,7 +67,9 @@ class PaymentsComponentTestCase extends CakeTestCase {
  * @return void
  */
 	public function testRedirect() {
-
+		$this->Payments->PaypalExpress->expects($this->once())->method('getUrlForToken')->with($this->equalTo('TOKEN'))->will($this->returnValue('URL'));
+		$this->Payments->controller->expects($this->once())->method('redirect')->with('URL');
+		$this->Payments->redirect('Paypal Express Checkout', array());
 	}
 
 /**
@@ -65,7 +78,8 @@ class PaymentsComponentTestCase extends CakeTestCase {
  * @return void
  */
 	public function testGetFromResponse() {
-
+		$value = $this->Payments->getFromResponse('Paypal Express Checkout', 'parameter');
+		$this->assertEqual($value, 'parameter_value');
 	}
 
 /**
