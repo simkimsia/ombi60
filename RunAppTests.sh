ff9e05c542e8d285e0f70c50fe30d8e4daa2c2f7
Graham Weldon <graham@grahamweldon.com>
2011-11-29 16:01:37 +1100

    Added RunAppTests.sh shell script to simplify testing.

---
+++ RunAppTests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp app/Config/core.php app/Config/core.php.backup
+echo "Configure::write('Acl.database', 'test');" >> app/Config/core.php
+lib/Cake/Console/cake testsuite app AllTests
+mv app/Config/core.php.backup app/Config/core.php
