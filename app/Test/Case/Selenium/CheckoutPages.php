16074af8a7b17ae8dcfc1861966accbbdbebd161
kimsia <kimcity@gmail.com>
2011-12-13 20:57:03 +0800

    remove duplicated tests in CheckoutPages and IsolatedNormalPagesTest for Selenium

--- app/Test/Case/Selenium/CheckoutPages.php
+++ app/Test/Case/Selenium/CheckoutPages.php
@@ -92,59 +92,6 @@ class CheckoutPagesTest extends PHPUnit_Extensions_SeleniumTestCase
 		
 		return true;
 	}
-
-	/**
-	 *
-	 * This replaces the OrdersController test for Redirect for create_order action
-	 *
-	 *
-	**/
-	public function testPaypalShouldWorkForGuestCustomerOpeningSingleStoreInBrowser() {
-		
-		if ($this->doNotRunThisTest(__FUNCTION__)) {
-			return;
-		}
-		
-		// GIVEN we have logged in to Sandbox Paypal
-		$this->paypalSelenium->loginToSandbox($this);
-		
-		// AND we are at checkout page 1 aka carts view action ie https://checkout.ombi60.localhost/carts/2/4e895a91-b374-4a1a-947c-0b701507707a
-		$this->open($this->baseCheckoutUrl . 'carts/2/4e895a91-b374-4a1a-947c-0b701507707a');
-		
-		$this->assertElementPresent('xpath=//span[@class="font_bold"][contains(text(), "You are using our secure server")]');
-		
-		// AND we fill in the form correctly
-		$this->type('id=UserEmail', 'guest_customer@ombi60.com');
-		$this->type('id=BillingAddress0FullName', 'G. Cherry');
-		$this->type('id=BillingAddress0Address', 'Billing Address St. Block 123 #01-911');
-		$this->type('id=BillingAddress0City', 'Singapore');
-		$this->type('id=BillingAddress0ZipCode', '11111');
-		$this->select('id=BillingAddress0Country', 'label=Singapore');
-		
-		// AND submit to create_order action
-		$this->assertElementPresent('xpath=//form[@action="/carts/2/4e895a91-b374-4a1a-947c-0b701507707a/create_order"]');
-		$this->clickAndWait('css=input[type="submit"]');
-		
-		// AND we reach pay action
-		$this->assertLocation('regexp:' . $this->baseCheckoutUrl . 'orders/2/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/pay');
-		
-		// AND we select Paypal
-		$this->click('id=PaymentShopsPaymentModuleId2', 'type=radio');
-		
-		// AND we click on complete purchase
-		$this->click('css=input[type="submit"][value="Complete my purchase"]');
-		
-		// AND we wait for up to 30 seconds for the sandbox page to show up
-		$this->waitForPageToLoad(30000);
-		
-		// WHEN we login to pay for the item at sandbox paypal
-		$this->paypalSelenium->loginToPayAtSandbox($this);
-		
-		// THEN we reach the success page
-		$this->assertLocation('regexp:' . $this->baseCheckoutUrl . 'orders/2/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/completed');
-		
-	}
-
  
     
 }
