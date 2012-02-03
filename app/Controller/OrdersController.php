a8613098af4eedf09df121061d8587a150484a42
Alejandro Ibarra <ajibarra@gmail.com>
2012-02-03 12:51:48 +0100

    Moving export logic to controller

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -288,7 +288,27 @@ class OrdersController extends AppController {
 		
 		return ($shopId === $order_shop_id);
 	}
-	
+
+	function admin_export($id = null) {
+		header("Content-type: text/csv");
+		header("Cache-Control: no-store, no-cache");
+		header('Content-Disposition: attachment; filename="filename.csv"');
+
+		$outstream = fopen("php://output",'w');
+
+		$test_data = array(
+			array( 'Cell 1,A', 'Cell 1,B' ),
+			array( 'Cell 2,A', 'Cell 2,B' )
+		);
+
+		foreach( $test_data as $row )
+		{
+			fputcsv($outstream, $row, ',', '"');
+		}
+
+		fclose($outstream);
+	}
+
 	public function admin_view($id = null) {
 		$this->log($this->request->params);
 					$this->log($this->layout);
