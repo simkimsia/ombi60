dd337767da0b30c642bc8d63c2e913cd795ee06e
kimsia <kimcity@gmail.com>
2012-02-04 15:34:31 +0800

    Integrate Alejandro treatment of csv output in controller action to render a export for order view

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -314,23 +314,89 @@ class OrdersController extends AppController {
 			'Payment Status',
 			'Fulfillment Status',
 			'Currency',
+			'Subtotal',
+			'Shipping Fee',
 			'Total',
+			'Shipping Method',
 			'Created at',
+			'Lineitem Name', 'Lineitem Price', 'Lineitem Quantity', 
+			'Billing Name',
 			'Billing Address',
+			'Billing Region',
+			'Billing Zip Code',
+			'Billing Country',
+			'Billing Phone',
+			'Shipping Name',
 			'Shipping Address',
+			'Shipping Region',
+			'Shipping Zip Code',
+			'Shipping Country',
+			'Shipping Phone',
 			'Note'
 		);
+
+		
+		App::uses('CsvLib', 'UtilityLib.Lib');
+		
 		$firstRow = array(
 			$order['Order']['order_no'],
 			$order['Order']['contact_email'],
 			$this->Order->Payment->getStatusNameGiven($order['Order']['payment_status']),
-			$order['Order']['net_amount']
+			$this->Order->Shipment->getStatusNameGiven($order['Order']['fulfillment_status']),
+			$order['Order']['currency'],
+			$order['Order']['amount'],
+			$order['Order']['shipping_fee'],
+			$order['Order']['net_amount'],
+			$order['Shipment'][0]['name'],
+			$order['Order']['created'],
+			
+			$order['OrderLineItem'][0]['variant_title'], $order['OrderLineItem'][0]['product_price'], $order['OrderLineItem'][0]['product_quantity'],
+
+			$order['BillingAddress']['full_name'],
+			$order['BillingAddress']['address'],
+			$order['BillingAddress']['region'],
+			$order['BillingAddress']['zip_code'],
+			$order['BillingAddress']['Country']['printable_name'],
+			$order['DeliveryAddress']['full_name'],			
+			$order['DeliveryAddress']['address'],
+			$order['DeliveryAddress']['region'],
+			$order['DeliveryAddress']['zip_code'],
+			$order['DeliveryAddress']['Country']['printable_name'],
+			CsvLib::escape($order['Order']['note'])
+			
 		);
 		
 		$test_data = array(
 			$columnHeaders,
 			$firstRow
 		);
+		
+		$lengthOfItems = count($order['OrderLineItem']);
+		if ($lengthOfItems > 1) {
+			$lineItemIndex = array_search('Lineitem Name', $columnHeaders);
+			$lineItemPriceIndex = $lineItemIndex + 1;
+			$lineItemQuantityIndex = $lineItemIndex + 2;
+			
+			$counter = 1;
+			while($counter < $lengthOfItems) {
+				$row = array(
+					0=>$order['Order']['order_no'],
+					1=>$order['Order']['contact_email'],
+					
+				);
+				// put empty columns
+				for($start = 2; $start < $lineItemIndex; $start++) {
+					$row[$start] = '';
+				}
+				
+				$row[$lineItemIndex] = $order['OrderLineItem'][$counter]['variant_title'];
+				$row[$lineItemPriceIndex] = $order['OrderLineItem'][$counter]['product_price'];
+				$row[$lineItemQuantityIndex] = $order['OrderLineItem'][$counter]['product_quantity'];
+				// now append to the test_data
+				$test_data[] = $row;
+				$counter++;
+			}
+		}
 
 		foreach( $test_data as $row )
 		{
@@ -352,7 +418,6 @@ class OrdersController extends AppController {
 		$module_name = "";
 		$order = $this->Order->getDetailed($id);
 		
-		$this->log($order);
 
 		if (!empty($order) && isset($order['Payment'][0]['shops_payment_module_id'])) {
 		    $shops_payment_module_id = $order['Payment'][0]['shops_payment_module_id'];
@@ -374,9 +439,8 @@ class OrdersController extends AppController {
 		if (isset($this->request->params['ext']) && $this->request->params['ext'] == 'csv') {
 			$this->layoutPath = 'csv';
 			$this->layout = 'empty';
-			$this->log('enter here');
 		}
-				$this->render('admin_view_term');
+		$this->render('admin_view_term');
 	}
 	
 	public function view($id = null) {
